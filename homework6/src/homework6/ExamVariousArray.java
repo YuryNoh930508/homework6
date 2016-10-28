29661fe2fcf85d54631d75803b7f7f8b396a64e1
노윤환 <@DESKTOP-55O4AC1>
2016-10-29 00:51:21 +0900

    CREATE "ExamVariousArray class file"
    메인함수 포함하는 클래스

---
+++ homework6/src/homework6/ExamVariousArray.java
@@ -0,0 +1,48 @@
+package homework6;
+import java.util.*;
+
+public class ExamVariousArray {
+	public static void main(String[] args) {
+		int selectedModule;
+		
+		Scanner s = new Scanner(System.in);
+		
+	    do{	    	
+	    	System.out.println("1. 두번째로 큰 수 찾기");
+	    	System.out.println("2. 심사 점수 계산");
+	    	System.out.println("3. 학생 총점, 평균값 구하기");
+	    	System.out.println("4. 5층 아파트의 거주자 숫자 구하기");
+	    	System.out.println("5. 겹치지 않는 숫자 10개입력받기");
+	    	System.out.println("6. 종료하기");
+	    	System.out.print("원하는 메뉴는>> ");
+	    	
+	    	if(s.hasNextInt())
+	    		selectedModule = s.nextInt();
+	    	else
+	    		continue;
+	    	
+	    	System.out.print("\n");
+	    	
+	    	switch(selectedModule) {
+	    		case 1: 
+	    			new SecondMax().init();
+	    			break;
+	    		case 2:
+	    			new AvgScore().init();
+	    			break;
+	    		case 3:
+	    			new StudentScore().init();
+	    			break;
+	    		case 4:
+	    			new AptPersonScore().init();
+	    			break;
+	    		case 5:
+	    			new OnlyNumber().init();
+	    			break;
+	    	}
+	    		
+	    } while(selectedModule!=6);
+	    
+	    System.out.println("종료하셨습니다.");
+	}
+}
